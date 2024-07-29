5149c2f46e27e48291b119759e383677f43bc7c6
Nícolas K. <87534075+Nicolas-Kelvin@users.noreply.github.com>
2024-07-29 16:18:52 -0300

    add ex

--- ex16/script.js
+++ ex16/script.js
@@ -1,25 +1,36 @@
-function contar() {
-  var ini = document.getElementById("txti");
-  var fim = document.getElementById("txtf");
-  var passo = document.getElementById("txtp");
-  var res = document.getElementById("res");
-  if (
-    ini.value.length == 0 ||
-    fim.value.length == 0 ||
-    passo.value.length == 0
-  ) {
-    window.alert("[ERROR] Faltam dados!");
-  } else {
-    res.innerHTML = "Contando: ";
-    var i = Number(ini.value);
-    var f = Number(fim.value);
-    var p = Number(passo.value);
+document.addEventListener("DOMContentLoaded", function() {
+  document.getElementById('btnContar').addEventListener('click', contar)
 
-    for (var c = i; c <= f; c += p) {
-      res.innerHTML += `${c} `;
+  function contar() {
+    const ini = document.getElementById('txti').value
+    const fim = document.getElementById('txtf').value
+    const passo = document.getElementById('txtp').value
+    const res = document.getElementById('res')
+
+    if (!ini || !fim || !passo) {
+      alert("[ERROR] Faltam dados!")
+      res.innerHTML = 'Impossivel contar!'
+      return;
     }
+
+    res.innerHTML = 'Contando: '
+    let i = Number(ini)
+    let f = Number(fim)
+    let p = Number(passo)
+
+    if (p <= 0) {
+      alert("[ERROR] Passo inválido! Considere PASSO 1")
+      p = 1
+    }
+
+    const step = i < f ? p : -p;
+    for (let c = i; (step > 0 ? c <= f : c >= f); c += step) {
+      res.innerHTML += `${c} \u{1F449}`
+    }
+    res.innerHTML += `\u{1F3C1}`
   }
-}
+});
+
 /*TESTE
     if (ini.value.length == 0 || passo.value.length == 0) {
         window.alert('[ERROR] Faltam dados!')
